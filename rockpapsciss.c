6c9d29858e2f601f0483da4b238e8201ae222576
Maria Braga <119708355+mariabraga15@users.noreply.github.com>
2022-12-03 15:51:54 +0200

    Add files via upload

---
+++ rockpapsciss.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+int main(){
+char game[1000][3];
+int n,i,j,op_score=0,my_score=0,valid=1;
+printf("Give the number of rounds you want to play: ");
+scanf("%d",&n);
+printf("\n");
+for(i=0;i<n;i++)
+   {
+    printf("game[%d][0]=",i);
+    scanf("%s",&game[i][0]);
+    printf("game[%d][1]=",i);
+    printf("\n");
+    scanf("%s",&game[i][1]);}//end scanning
+for( i=0;i<n&&(valid==1);i++)
+{switch(game[i][0]){
+case 'A': {op_score+=1; 
+if(game[i][1]=='X')
+op_score+=3; 
+else if(game[i][1]=='Z')
+op_score+=6;
+break;} //end A
+case 'B':{
+op_score+=2;
+if(game[i][1]=='Y')
+op_score+=3; 
+else if(game[i][1]=='X')
+op_score+=6;
+break;} //end B
+case 'C':{op_score+=3;
+if(game[i][1]=='Y')
+op_score+=6; 
+else if(game[i][1]=='Z')
+op_score+=3;
+ break;}//end C
+default: {valid=0;printf("The rounds are not valid\n"); break;}//Default case for switch1
+}//end switch1
+switch(game[i][1]){
+case 'X': {my_score+=1; 
+if(game[i][0]=='A')
+my_score+=3; 
+else if(game[i][0]=='C')
+my_score+=6;
+break;}//case X
+case 'Y':{ my_score+=2; 
+if(game[i][0]=='B')
+my_score+=3; 
+else if(game[i][0]=='A')
+my_score+=6;
+
+break;}//case Y
+case 'Z':{my_score+=3;
+if(game[i][0]=='B')
+my_score+=6; 
+else if(game[i][0]=='C')
+my_score+=3;
+break;}//case Z
+default: {valid=0; printf("The rounds are not valid\n"); break;}//default switch2
+
+}//end switch2
+}
+printf("The opponent's score is %d\n",op_score);
+printf("My score is %d\n",my_score); //displaying the scores for each player
+if(valid==0)
+printf("Invalid game");
+else
+if(op_score>my_score)
+printf("You lost the game");
+else if(op_score==my_score)
+printf("It's a draw!Choose another strategy!");
+else
+printf("You win the game");//the final conclusion
+return 0;
+}
+
